b321a9794b12275e25b2a61fecea9f01a9594546
dinitrogen <noshowdon@gmail.com>
2025-01-13 17:56:45 -0500

    completed training up to dynamic entries

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,22 +1,67 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Contact, Phone } from '../contacts/contact.model';
+import { ContactsService } from '../contacts/contacts.service';
+import { phoneTypeValues, addressTypeValues } from '../contacts/contact.model';
+import { RestrictedWordsValidator } from '../validators/restricted-words-validator.directive';
+import { DateValueAccessorDirective } from '../date-value-accessor/date-value-accessor.directive';
+import { ProfileIconSelectorComponent } from "../profile-icon-selector/profile-icon-selector.component";
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RestrictedWordsValidator, DateValueAccessorDirective, ProfileIconSelectorComponent, ProfileIconSelectorComponent],
   standalone: true,
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.css']
+  styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  phoneTypes = phoneTypeValues;
+  addressTypes = addressTypeValues;
+
+  contact: Contact = {
+    id: '',
+    icon: '',
+    personal: false,
+    firstName: '',
+    lastName: '',
+    dateOfBirth: null,
+    favoritesRanking: 0,
+    phones: [{
+      phoneNumber: '',
+      phoneType: '',
+    }],
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+    notes: ''
+  };
+
+  constructor(private route: ActivatedRoute, private contactsService: ContactsService, private router: Router) {}
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
-    if (!contactId) return
+    if (!contactId) return;
+    this.contactsService.getContact(contactId).subscribe(contact => {
+      if (contact) {
+        this.contact = contact;
+      }
+    });
   }
 
-  saveContact() {
+  saveContact(form: NgForm) {
+    console.log(form.value);
+    this.contactsService.saveContact(this.contact).subscribe({
+      next: () => this.router.navigate(['/contacts'])
+    });
 
+  }
+
+  addPhoneFields() {
+    this.contact.phones.push({phoneNumber: '', phoneType: ''});
   }
 }
